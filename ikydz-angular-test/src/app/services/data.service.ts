8a0071cfa95e73725113cad19ebdf685b49739ee
VladKozlov863 <01gadhakim@gmail.com>
2020-07-24 16:27:12 +0300

    feat(services): added a constant with an empty string to initialize the default search.

--- ikydz-angular-test/src/app/services/data.service.ts
+++ ikydz-angular-test/src/app/services/data.service.ts
@@ -1,5 +1,6 @@
 import {Injectable} from '@angular/core';
 import {BehaviorSubject} from 'rxjs';
+import {DEFAULT_SEARCH_STRING} from './utils.service';
 
 
 @Injectable({
@@ -7,10 +8,10 @@ import {BehaviorSubject} from 'rxjs';
 })
 export class DataService {
 
-  public countryNameSource = new BehaviorSubject<string>('');
+  public countryNameSource = new BehaviorSubject<string>(DEFAULT_SEARCH_STRING);
   public currentCountryName = this.countryNameSource.asObservable();
 
   public changeCountryName(country: string): void {
-    // complete this function to notify components
+    this.countryNameSource.next(country);
   }
 }
--- ikydz-angular-test/src/app/services/utils.service.ts
+++ ikydz-angular-test/src/app/services/utils.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 
+export const DEFAULT_SEARCH_STRING = '';
+
 @Injectable({
   providedIn: 'root'
 })
